4d7a86f444e353a5285e24e06fedf96a6a10efb3
susan <susan@users.noreply.github.com>
2019-05-05 09:54:02 -0400

    images

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Report.destroy_all
 
 Report.create(
 title: "Bluelinx Holdings Inc com",
-image: "image/Bluelinx.jpg",
+image: "https://pbs.twimg.com/media/DYGffj9WsAAcNIt.jpg",
 description: "Selling by legacy PE owner means opportunity to buy with 100% upside.",
 pricing_details: "LONG",
 recent_price: 15.79,
@@ -21,7 +21,7 @@ target_price: 20.00
 
 Report.create(
 title: "Charter Communications Inc A",
-image: "image/Charter.jpg",
+image: "https://pbs.twimg.com/media/DOS9JWwUEAA7cIL.jpg",
 description: "Misunderstanding of profit drivers creates big opportunity to buy at attractive price",
 pricing_details: "LONG",
 recent_price: 341.20,
@@ -30,7 +30,7 @@ target_price: 540.00
 
 Report.create(
  title: "Viad Corp",
- image: "image/Viad.jpg",
+ image: "https://pbs.twimg.com/media/DO2tHB9WkAAwtin.jpg",
  description: "Viad is a combination of of two unrelated units with 100% upside within 2 years" ,
  pricing_details: "LONG",
  recent_price: 55.95,
@@ -39,7 +39,7 @@ Report.create(
 
 Report.create(
  title: "On the Beach Group plc",
- image: "image/On The Beach jpg",
+ image: "https://pbs.twimg.com/media/DP-CVYGXkAYo6YB.jpg",
  description: "Operating in a growing market against inefficicent competitors",
  pricing_details: "LONG",
  recent_price: 386.00,
@@ -48,7 +48,7 @@ Report.create(
 
 Report.create(
  title: "Tesaro Inc Com",
- image: "image/Tesaro Inc.jpg",
+ image: "https://pbs.twimg.com/media/Dtf46b2UwAAzJa_.jpg",
  description: "Trading at 45% discount to conservatively arrived at Takeout Value",
  pricing_details: "LONG",
  recent_price: 43.01,
@@ -56,7 +56,7 @@ Report.create(
  	)
 Report.create(
  title: "Liberty Tax Inc",
- image: "image/Liberty Tax.jpg",
+ image: "https://pbs.twimg.com/media/DtHr3jQXoAE49Mg.jpg",
  description: "Re-listing coming, possible buyout",
  pricing_details: "LONG",
  recent_price: 10.01,
@@ -64,7 +64,7 @@ Report.create(
  	)
 Report.create(
  title: "Dexcom Inc Com",
- image: "image/Dexcom Inc.jpg",
+ image: "https://pbs.twimg.com/media/Dj77OqsX0AAt95P.jpg",
  description: "Poised for accelerating penetration across several massive TAMS" ,
  pricing_details: "LONG",
  recent_price: 123.33,
@@ -72,7 +72,7 @@ Report.create(
  	)
 Report.create(
  title: "Horizon Global Corp Com",
- image: "image/Horizon.jpg",
+ image: "https://pbs.twimg.com/media/DRqJ4TBU8AAJmfu.jpg",
  description: "Experiencing expanding margins, trading at less than 5.0 EV/2019 EBITDA",
  pricing_details: "LONG",
  recent_price: 14.45,
@@ -80,7 +80,7 @@ Report.create(
  	)
 Report.create(
  title: "Cheniere Energy Inc.",
- image: "image/Cheniere.jpg",
+ image: "https://pbs.twimg.com/media/DYG1f2oW0AAbL5G.jpg",
  description: "Upside from continued execution in market with attractive LT dynamics" ,
  pricing_details: "LONG",
  recent_price: 54.04,
@@ -88,7 +88,7 @@ Report.create(
  	)
 Report.create(
  title: "Nexstar Media Group Inc. cl A",
- image: "image/Nexstar.jpg",
+ image: "https://pbs.twimg.com/media/DlsBeGqWwAATGUb.jpg",
  description: "Catalysts from upcoming election and renewed private equity interest",
  pricing_details: "LONG",
  recent_price: 82.30,
@@ -96,7 +96,7 @@ Report.create(
  	)
 Report.create(
  title: "B&G Foods Inc",
- image: "image/B&G.jpg",
+ image: "https://pbs.twimg.com/media/DNuAgAYW0AAXsWv.jpg",
  description: "Historically well-run and in doghouse after a few misses--should be temporary and get paid 6% while waiting",
  pricing_details: "LONG",
  recent_price: 32.70,
